ff389fd370eadcabed2cbe897f0706d2c21cc56a
André Nicoletti <andregnicoletti@gmail.com>
2025-05-28 01:00:47 -0300

    Rest controller tested

---
+++ src/main/java/com/nicoletti/store/controllers/CategoriaController.java
@@ -0,0 +1,17 @@
+package com.nicoletti.store.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Teste ok");
+    }
+
+}
