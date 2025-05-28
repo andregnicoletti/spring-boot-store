e38ee754c16f1678d6a3f6abfc3fd6086ba6729b
André Nicoletti <andregnicoletti@gmail.com>
2025-05-28 10:32:10 -0300

    created city and state repository

---
+++ src/main/java/com/nicoletti/store/repositories/StateRepository.java
@@ -0,0 +1,9 @@
+package com.nicoletti.store.repositories;
+
+import com.nicoletti.store.entities.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Long> {
+}
